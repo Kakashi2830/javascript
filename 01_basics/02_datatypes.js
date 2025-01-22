92e35574eee8e4d2f56896addd2b8d0cddcc0150
Sagnik Nandi <145244192+Kakashi2830@users.noreply.github.com>
2025-01-22 07:35:13 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict";// treat all JS code as newer version
+
+//alert(3+3)//we are using nodejs, not browser
+
+//code readability should be high
+
+console.log(3+3)
+console.log("Sagnik")
+
+let name="Sagnik"
+let age=21
+let isLoggedIn=false
+
+// number => 2^53 range
+// bigInt
+// string => ""
+// boolean => true/false
+// null => standalone value/object
+// undefined => it's undefined
+// symbol 
+
+
+// object 
+
+console.log(typeof "hitesh");

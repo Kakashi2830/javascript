3e58064804c5e3731047b8a8d081c64dc0418257
Sagnik Nandi <145244192+Kakashi2830@users.noreply.github.com>
2025-01-23 05:57:56 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,28 @@
+//console.log(2>1);
+//console.log(2>=1);
+//console.log(2<1);
+//console.log(2==1);
+//console.log(2!=1);
+
+//console.log("2">1);
+//console.log("02">1);
+
+
+//avoid this kind of conversions
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+
+console.log(undefined==0);
+console.log(undefined>0);
+console.log(undefined<0);
+
+// === strict check also checks data type with equality
+
+console.log("2"===2);
+
+
+
+
+
